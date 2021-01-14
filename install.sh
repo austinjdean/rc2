31a549149a702641dd9dc53c409d41f2aabadf67
rt-adean <root@example.com>
2021-01-14 11:13:40 -0500

    add jq to install list

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 repo_name="rc2"
 git_dir="$HOME/git"
 package_dir="$git_dir/$repo_name/nice_package"
-repo_programs="zsh vim guake sublime-text xclip nmap dconf-cli"
+repo_programs="zsh vim guake sublime-text xclip nmap dconf-cli jq"
 default_directories="Desktop Documents Downloads Music Pictures Public Templates Videos"
 
 # Add Sublime to repos and update apt sources
